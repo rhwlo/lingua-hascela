ffd7a26264f50f940be763a5d57b48ea9c9a2a47
Joshua Coats <joshu@fearchar.net>
2015-09-15 10:50:18 -0700

    Adding the groundwork for using Whitaker Rules

--- Data/Latin/Internal.hs
+++ Data/Latin/Internal.hs
@@ -1,5 +1,15 @@
 module Data.Latin.Internal where
 
+import qualified Data.Map as DM
+
+type ConjugationRules = DM.Map ConjugationTuple (VerbalForm, String)
+
+type ConjugationTuple = ((Int, Int), VerbTense, VerbVoice, VerbMood, Int, NounNumber)
+
+type DeclensionRules  = DM.Map DeclensionTuple  (NounCase, String)
+
+type DeclensionTuple  = ((Int, Int), NounCase, NounNumber, NounGender)
+
 data Noun = Noun { declension   :: (Int, Int),
                    nomStem      :: String,              -- the Nominative stem
                    intactNom    :: Bool,                -- whether the Nominative stem needs an ending
@@ -14,9 +24,13 @@ data NounCase = Ablative
               | Nominative
               | Vocative deriving (Show, Eq, Read)
 
-data NounGender = Feminine
+data NounGender = Common
+                | Feminine
                 | Masculine
                 | Neuter deriving (Show, Eq, Read)
+
+data NounNumber = Singular
+                | Plural deriving (Show, Eq, Read, Ord)
 
 data Verb = Verb (Int, Int)         -- the conjugation
                  VerbalCategory
@@ -27,6 +41,18 @@ data Verb = Verb (Int, Int)         -- the conjugation
                  (Maybe String)     -- perfect passive stem
                  deriving (Show, Eq)
 
+data VerbTense = Present
+               | Imperfect
+               | Future
+               | Perfect
+               | Pluperfect
+               | Futureperfect deriving (Show, Eq, Ord, Read)
+
+data VerbVoice = Active
+               | Passive deriving (Show, Eq, Ord, Read)
+
+data VerbMood = Indicative
+              | Subjunctive deriving (Show, Eq, Ord, Read)
 
 data VerbalCategory = Deponent
                     | Intransitive
--- Data/Latin/Whitaker.hs
+++ Data/Latin/Whitaker.hs
@@ -4,6 +4,10 @@ where
 import Data.Latin.Internal (
   NounCase(..),
   NounGender(..),
+  NounNumber(..),
+  VerbMood(..),
+  VerbTense(..),
+  VerbVoice(..),
   VerbalCategory(..),
   VerbalForm(..))
 
@@ -24,6 +28,20 @@ data WhitakerLine = WV { wvStem       :: String,
                          wnCase       :: NounCase,
                          wnIntactStem :: Bool,
                          wnId         :: Int }
+                  | WNR { wnrDecl     :: (Int, Int),
+                          wnrCase     :: NounCase,
+                          wnrNumber   :: NounNumber,
+                          wnrGender   :: NounGender,
+                          wnrStemType :: NounCase,
+                          wnrEnding   :: String }
+                  | WVR { wvrConj     :: (Int, Int),
+                          wvrTense    :: VerbTense,
+                          wvrVoice    :: VerbVoice,
+                          wvrMood     :: VerbMood,
+                          wvrPerson   :: Int,
+                          wvrNumber   :: NounNumber,
+                          wvrStemType :: VerbalForm,
+                          wvrEnding   :: String }
                   | WUnparseable deriving (Show, Eq)
 
 getWId :: WhitakerLine -> Int
@@ -34,18 +52,6 @@ getWId _ = 0
 getWForm :: WhitakerLine -> VerbalForm
 getWForm (WV _ _ _ wForm _) = wForm
 getWForm _ = OtherVerbalForm
-
-parseWhitakerLine :: String -> WhitakerLine
-parseWhitakerLine line = case words line of
-    [stem, "V", conjOneS, conjTwoS, vCatS, vFormS, idS] ->
-      WV stem (read conjOneS, read conjTwoS) (readvCat vCatS) (readvForm vFormS) (read idS)
-    [stem, "N", declOneS, declTwoS, nGenderS, [nKind], nCaseS, idS] ->
-      let intactStem = nCaseS == "1" in
-        WN stem (read declOneS, read declTwoS)
-           (readnGender nGenderS) nKind (readnCase nCaseS)
-           intactStem (read idS)
-    _ ->
-      WUnparseable
 
 readvCat :: String -> VerbalCategory
 readvCat "DEP"      = Deponent
@@ -62,60 +68,120 @@ readvForm _         = PresActiveStem
 readnGender :: String -> NounGender
 readnGender "F"     = Feminine
 readnGender "M"     = Masculine
-readnGender _       = Neuter
+readnGender "N"     = Neuter
+readnGender _       = Common        -- usually "C", sometimes "X"
 
 readnCase :: String
           -> NounCase
+readnCase "ABL"     = Ablative
+readnCase "ACC"     = Accusative
+readnCase "DAT"     = Dative
+readnCase "GEN"     = Genitive
+readnCase "VOC"     = Vocative
 readnCase "2"       = Genitive
 readnCase _         = Nominative
+
+readnNumber :: String
+            -> NounNumber
+readnNumber "S"     = Singular
+readnNumber _       = Plural
+
+readvMood :: String
+          -> VerbMood
+readvMood "SUB" = Subjunctive
+readvMood _     = Indicative
+
+readvTense :: String
+           -> VerbTense
+readvTense "IMPF" = Imperfect
+readvTense "FUT"  = Future
+readvTense "PERF" = Perfect
+readvTense "PLUP" = Pluperfect
+readvTense "FUTP" = Futureperfect
+readvTense _      = Present
+
+readvVoice :: String
+           -> VerbVoice
+readvVoice "PASSIVE" = Passive
+readvVoice _         = Active
+
+
+parseWhitakerLine :: String -> WhitakerLine
+parseWhitakerLine line = case words line of
+    [stem, "V", conjOneS, conjTwoS, vCatS, vFormS, idS] ->
+      WV stem (read conjOneS, read conjTwoS) (readvCat vCatS) (readvForm vFormS) (read idS)
+    [stem, "N", declOneS, declTwoS, nGenderS, [nKind], nCaseS, idS] ->
+      let intactStem = nCaseS == "1" in
+        WN stem (read declOneS, read declTwoS)
+           (readnGender nGenderS) nKind (readnCase nCaseS)
+           intactStem (read idS)
+    "N":(declOneS:(declTwoS:(rCaseS:(rGenderS:(rNumberS:(rStemTypeS:(rEndingS:_))))))) ->
+      WNR (read declOneS, read declTwoS)
+          (readnCase rCaseS)
+          (readnNumber rNumberS)
+          (readnGender rGenderS)
+          (readnCase rStemTypeS)
+          (if length (words line) < 11
+            then ""
+            else rEndingS)
+    "V":(conjOneS:(conjTwoS:(rTenseS:(rVoiceS:(rMoodS:(rPersonS:(rNumberS:(rStemTypeS:(_:(rEnding:_)))))))))) ->
+      WVR (read conjOneS, read conjTwoS)
+          (readvTense rTenseS)
+          (readvVoice rVoiceS)
+          (readvMood rMoodS)
+          (read rPersonS)
+          (readnNumber rNumberS)
+          (readvForm rStemTypeS)
+          rEnding
+    _ ->
+      WUnparseable
 
 whitakerLinesToDLI :: [WhitakerLine]            -- the Whitaker Lines to convert into Data.Latin
                    -> ([DLI.Noun],              -- the Whitaker Lines that converted successfully into Nouns
                        [DLI.Verb])              -- the Whitaker Lines that converted successfuly into Verbs
-whitakerLinesToDLI fullWLines = (nouns, verbs)
+whitakerLinesToDLI fullWLines = (dliNouns, dliVerbs)
   where
-    (_, nouns, verbs) = foldl' convertLines ([],[],[]) $ sortBy (comparing getWId) fullWLines
-
-convertLines :: ([WhitakerLine], [DLI.Noun], [DLI.Verb])
-              -> WhitakerLine
-              -> ([WhitakerLine], [DLI.Noun], [DLI.Verb])
-convertLines (digerenda, nouns, verbs) wLine = let
-    lastLine = maybeHead digerenda
-  in
-    if fmap getWId lastLine /= Just (getWId wLine)
-      then
-        case lastLine of
-          Just (WV {}) ->
-            ([wLine], nouns, makeVerb digerenda : verbs)
-          Just (WN {}) -> case makeNoun digerenda of
-            Just newNoun -> ([wLine], newNoun : nouns, verbs)
-            Nothing -> ([wLine], nouns, verbs)
-          _ ->
-            ([wLine], nouns, verbs)
-      else
-        (wLine:digerenda, nouns, verbs)
-  where
-    makeNoun :: [WhitakerLine] -> Maybe DLI.Noun
-    makeNoun wLines = let
-        maybeNom = maybeHead $ filter ((Nominative ==) . wnCase) wLines
-        maybeGen = maybeHead $ filter ((Genitive ==) . wnCase) wLines
+    (_, dliNouns, dliVerbs) = foldl' convertLines ([],[],[]) $ sortBy (comparing getWId) fullWLines
+    convertLines :: ([WhitakerLine], [DLI.Noun], [DLI.Verb])
+                  -> WhitakerLine
+                  -> ([WhitakerLine], [DLI.Noun], [DLI.Verb])
+    convertLines (digerenda, nouns, verbs) wLine = let
+        lastLine = maybeHead digerenda
       in
-        maybeNom >>= \nom ->
-          return $ DLI.Noun (wnDecl nom)
-                            (wnStem nom)
-                            (wnIntactStem nom)
-                            (wnStem <$> maybeGen)
-                            (wnGend nom)
-    makeVerb :: [WhitakerLine] -> DLI.Verb
-    makeVerb wLines = let
-        maybePresActive = wvStem <$> maybeHead (filter ((PresActiveStem ==) . wvForm) wLines)
-        maybeInfinitive = wvStem <$> maybeHead (filter ((InfinitiveStem ==) . wvForm) wLines)
-        maybePresPassive = wvStem <$> maybeHead (filter ((PresPassiveStem ==) . wvForm) wLines)
-        maybePastActive = wvStem <$> maybeHead (filter ((PastActiveStem ==) . wvForm) wLines)
-        maybePastPassive = wvStem <$> maybeHead (filter ((PastPassiveStem ==) . wvForm) wLines)
-        WV _ vConj vCat _ _ : _ = wLines
-      in
-        DLI.Verb vConj vCat maybePresActive maybeInfinitive maybePresPassive maybePastActive maybePastPassive
+        if fmap getWId lastLine /= Just (getWId wLine)
+          then
+            case lastLine of
+              Just (WV {}) ->
+                ([wLine], nouns, makeVerb digerenda : verbs)
+              Just (WN {}) -> case makeNoun digerenda of
+                Just newNoun -> ([wLine], newNoun : nouns, verbs)
+                Nothing -> ([wLine], nouns, verbs)
+              _ ->
+                ([wLine], nouns, verbs)
+          else
+            (wLine:digerenda, nouns, verbs)
+      where
+        makeNoun :: [WhitakerLine] -> Maybe DLI.Noun
+        makeNoun wLines = let
+            maybeNom = maybeHead $ filter ((Nominative ==) . wnCase) wLines
+            maybeGen = maybeHead $ filter ((Genitive ==) . wnCase) wLines
+          in
+            maybeNom >>= \nom ->
+              return $ DLI.Noun (wnDecl nom)
+                                (wnStem nom)
+                                (wnIntactStem nom)
+                                (wnStem <$> maybeGen)
+                                (wnGend nom)
+        makeVerb :: [WhitakerLine] -> DLI.Verb
+        makeVerb wLines = let
+            maybePresActive = wvStem <$> maybeHead (filter ((PresActiveStem ==) . wvForm) wLines)
+            maybeInfinitive = wvStem <$> maybeHead (filter ((InfinitiveStem ==) . wvForm) wLines)
+            maybePresPassive = wvStem <$> maybeHead (filter ((PresPassiveStem ==) . wvForm) wLines)
+            maybePastActive = wvStem <$> maybeHead (filter ((PastActiveStem ==) . wvForm) wLines)
+            maybePastPassive = wvStem <$> maybeHead (filter ((PastPassiveStem ==) . wvForm) wLines)
+            WV _ vConj vCat _ _ : _ = wLines
+          in
+            DLI.Verb vConj vCat maybePresActive maybeInfinitive maybePresPassive maybePastActive maybePastPassive
 
 maybeHead :: [a] -> Maybe a
 maybeHead []    = Nothing
